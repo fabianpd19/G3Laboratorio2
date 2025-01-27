454c85a86ba7c12e72565366f5031d820aefdb2d
Edison Arambulo crack <eaar2300appol@gmail.com>
2025-01-26 23:16:00 -0500

    Implementación de carrito flotante y estilos responsivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { Product } from './product';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductListComponent, ProductDetailsComponent, ShoppingCartComponent], // Importa los componentes que se usen aquí
+  imports: [ProductListComponent, ProductDetailsComponent, ShoppingCartComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
   selectedProduct: Product | null = null;
   cart: Product[] = [];
+  isMobileView: boolean = window.innerWidth <= 768;
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isMobileView = window.innerWidth <= 1000;
+  }
 
   onSelectProduct(product: Product) {
     this.selectedProduct = product;
